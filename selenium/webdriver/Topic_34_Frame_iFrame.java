1106f3a505abe285685679f45842e07cf006fffb
Pham Tat Dat <tatdat.ph@gmail.com>
2023-04-27 07:01:36 +0700

    Apr 27

--- selenium/webdriver/Topic_34_Frame_iFrame.java
+++ selenium/webdriver/Topic_34_Frame_iFrame.java
@@ -1,11 +1,14 @@
 package webdriver;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.server.handler.SwitchToFrame;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -43,36 +46,49 @@ public class Topic_34_Frame_iFrame {
 		//So luong like
 		driver.switchTo().frame(frameElement);
 		
-		Assert.assertEquals(driver.findElement(By.xpath("//a[text()='Kyna.vn']/parent::div/following-sibling::div")).getText(), "165K likes");
+		Assert.assertEquals(driver.findElement(By.xpath("//a[text()='Kyna.vn']/parent::div/following-sibling::div")).getText(), "164K likes");
 		
 		// ve trang chu
 		driver.switchTo().defaultContent();
 		
 		//vao chat
-		driver.switchTo().frame("cs_chat_iframe");
-		driver.findElement(By.cssSelector("div#cs-live-chat")).click();
+		sleep(2);
 		
+		
+		driver.switchTo().frame("cs_chat_iframe");
+		driver.findElement(By.cssSelector("div.meshim_widget_Widget")).click();
+		
+		driver.findElement(By.cssSelector("input.input_name")).sendKeys("Pham Tat Dat");
+		driver.findElement(By.cssSelector("input.input_phone")).sendKeys("0987654321");
+		driver.findElement(By.cssSelector("select#serviceSelect")).click();
+		new Select(driver.findElement(By.cssSelector("select#serviceSelect"))).selectByVisibleText("HỖ TRỢ KỸ THUẬT");
+		sleep(3);
+	
+		driver.switchTo().defaultContent();
+		driver.findElement(By.cssSelector("input#live-search-bar")).sendKeys("Excel");
+		driver.findElement(By.cssSelector("button.search-button")).click();
+		
+		List<WebElement> danhSachExcel = driver.findElements(By.xpath("//img[contains(@alt,'Excel')]"));
+		
+		System.out.println(danhSachExcel.size());
 	}
 
 	@Test
-	public void TC_03_() {
-
+	public void TC_02_() {
+		driver.get("https://netbanking.hdfcbank.com/netbanking/");
+		
+		driver.switchTo().frame("login_page");
+		
+		driver.findElement(By.cssSelector("input.form-control")).sendKeys("autotest@gmail.net");
+		
+		driver.findElement(By.cssSelector("a.btn-primary")).click();
+		sleep(5);
+		
+		driver.switchTo().defaultContent();
+		
+		Assert.assertTrue(driver.findElement(By.cssSelector("input#keyboard")).isDisplayed());
 	}
 
-	@Test
-	public void TC_04_() {
-
-	}
-
-	@Test
-	public void TC_05_() {
-
-	}
-
-	@Test
-	public void TC_06_() {
-		sleep(1);
-	}
 
 	private void sleep(long timeout) {
 		try {
@@ -84,6 +100,6 @@ public class Topic_34_Frame_iFrame {
 
 	@AfterClass
 	public void afterClass() {
-		//driver.quit();
+		driver.quit();
 	}
 }
