1fee863ba60ded2f239e3f156d304587b96ab8cf
Minda <Minda@PHAM-TAT-DAT>
2023-06-02 22:34:38 +0700

    PC

--- selenium/webdriver/Topic_38_Wait_P2.java
+++ selenium/webdriver/Topic_38_Wait_P2.java
@@ -2,8 +2,11 @@ package webdriver;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -12,7 +15,7 @@ public class Topic_38_Wait_P2 {
 	WebDriver driver;
 	String projectPath = System.getProperty("user.dir");
 	String osName = System.getProperty("os.name");
-
+	WebDriverWait explicitWait;
 	@BeforeClass
 	public void beforeClass() {
 		if (osName.contains("Windows")) {
@@ -22,12 +25,16 @@ public class Topic_38_Wait_P2 {
 		}
 
 		driver = new FirefoxDriver();
+		
+		explicitWait = new WebDriverWait(driver, 15);
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 	}
 
 	@Test
 	public void TC_01_() {
+		driver.get("https://www.facebook.com/");
+		explicitWait.until(ExpectedConditions.elementToBeClickable(By.name("login"))).click();
 	}
 
 	@Test
@@ -41,7 +48,7 @@ public class Topic_38_Wait_P2 {
 	}
 
 	@Test
-	public void TC_04_() {
+	public void TC_04_() {   
 
 	}
 
